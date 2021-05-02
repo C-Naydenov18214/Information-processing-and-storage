caad13905922a15da24dd8ebb27a8a758fedaa67
TuS <tussss89@gmail.com>
2021-05-02 10:59:03 +0700

    works

--- WebApp/WebApplication/WebApplication/Controllers/QueryController.cs
+++ WebApp/WebApplication/WebApplication/Controllers/QueryController.cs
@@ -127,9 +127,91 @@ namespace WebApplication.Controllers
 
         [HttpGet]
         [ActionName("route")]
-        public string Route([FromQuery] string from, [FromQuery] string to)
+        public async Task<ActionResult<IEnumerable<KeyValuePair<string,int>>>> Route([FromQuery] string fromAirport, [FromQuery] string toAirport, [FromQuery] DateTime date,[FromQuery] string clazz, [FromQuery] int limit)
         {
-            return $"from {from} to {to}";
+            var flights = from route in db.Flights
+                          where route.DepartureAirport == fromAirport
+                          where route.ArrivalAirport == toAirport
+                          where route.ScheduledDeparture.Date >= date && route.ScheduledDeparture.Date <= date.AddDays(1)
+                          select route;
+
+            var ticketWithNecessaryClazz = from ticketFlight in db.TicketFlights
+                                           where ticketFlight.FareConditions == clazz
+                                           select ticketFlight;
+            var joined = from fl in flights
+                         join ticketFlight in ticketWithNecessaryClazz on fl.FlightId equals ticketFlight.FlightId
+
+                         select new
+                         {
+                             Flight = fl,
+                             TicketFlight = ticketFlight
+                         };
+            var grouped = from g in joined
+                          group g by g.Flight.FlightId;
+            var ticketsCounter = from g in grouped
+                         select new KeyValuePair<int,int>(g.Key, g.Count());
+
+            var plainFlightPair = from plain in db.AircraftsData
+                         join flight in flights on plain.AircraftCode equals flight.AircraftCode
+                         select new
+                         {
+                            Flight = flight,
+                            Plain = plain
+                         };
+
+            var seats = from pair in plainFlightPair
+                                   join seates in db.Seats on pair.Plain.AircraftCode equals seates.AircraftCode
+                                   where seates.FareConditions == clazz
+                                   group seates by seates.AircraftCode;
+            var seatsCounter = from s in seats
+                               select new KeyValuePair<string,int>(s.Key, s.Count());
+
+
+            /*var r1 = from pair in plainFlightPair
+                     join tc in ticketsCounter on pair.Flight.FlightId equals tc.Key
+                     //where tc.Elements[0] > 0
+                     select pair.Flight.FlightId;*/
+            
+            
+            /*var res = from pair in plainFlightPair
+                      join sc in seatsCounter on pair.Plain.AircraftCode equals sc.Key
+                      join tc in ticketsCounter on pair.Flight.FlightId equals tc.Key
+                      where Math.Abs(sc.Value - tc.Value) > 0
+                      select pair.Flight.FlightId;*///new HelpClasses.Flight(pair.Flight.ScheduledArrival.ToString(), pair.Flight.ScheduledArrival.ToString(), pair.Flight.FlightNo, pair.Flight.DepartureAirport, pair.Flight.ArrivalAirport) ;
+
+
+
+            return await seatsCounter.ToListAsync();
+        
+        }
+
+        [HttpGet]
+        [ActionName("booking")]
+
+        public bool Book()
+        {
+
+
+            return true;
+        
+        }
+
+        [HttpGet]
+        [ActionName("check")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> Check(string passengerID)
+        {
+            var res = from ticket in db.Tickets
+                      where ticket.PassengerId == passengerID
+                      select ticket;
+            if (res != null)
+            {
+                return await res.ToListAsync();
+            }
+            else
+            {
+                return null; 
+            }
+
         
         }
 
